dca725d555d6503a1f8369d3f3247c0a78e319a8
Tyler Sargent <tsargent@clicktherapeutics.com>
2022-08-10 17:01:49 -0400

    set an interval for tick

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,13 @@
+import { useEffect, useRef } from "react";
+
 const Game = () => {
+  const timer = useRef(null);
+
+  useEffect(() => {
+    timer.current = setInterval(() => {
+      console.log(Date.now());
+    }, 1000);
+  }, []);
   return <div>this is the game</div>;
 };
 
